9c57e4f6c34b27347c51399576b2071a83accaca
qq775193759 <775193759@qq.com>
2016-04-04 14:37:44 +0800

    all

---
+++ 2048_lu.sh
@@ -0,0 +1,1 @@
+build/X86_MESI_Two_Level/gem5.opt configs/example/fs.py  --kernel=x86_64-vmlinux-2.6.22.9-nolimit.smp  --caches --l2cache --l1i_size=32kB --l1d_size=32kB --l1d_assoc=2 --l1i_assoc=2 --l2_size=2048kB --l2_assoc=8 --script=script/lu_1c_2048k.rcS  --num-cpus=1 --num-dirs=1
