6709a1aa415e1871f461dc8a90e01abf0b4c4be8
qq775193759 <775193759@qq.com>
2016-04-04 14:04:03 +0800

    add something

---
+++ 4c_128_lu.sh
@@ -0,0 +1,1 @@
+build/X86_MESI_Two_Level/gem5.opt configs/example/fs.py  --kernel=x86_64-vmlinux-2.6.22.9-nolimit.smp  --caches --l2cache --l1i_size=32kB --l1d_size=32kB --l1d_assoc=2 --l1i_assoc=2 --l2_size=128kB --l2_assoc=8 --script=script/lu_4c_128k.rcS  --num-cpus=4 --num-dirs=4
